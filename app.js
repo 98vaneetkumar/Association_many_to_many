e1459fc6123adbd1b30491a8c85300887f3ddb51
98vaneetkumar <vaneet3157@gmail.com>
2022-10-22 16:10:15 +0530

    Association_many_to_many

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+
+const rout = require("./routess/router");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Home page");
+  console.log("vaneet kumar");
+});
+
+app.use("/", rout);
+
+app.listen(port, (req, res) => {
+  console.log(`listening in the port http://localhost:${port}`);
+});
